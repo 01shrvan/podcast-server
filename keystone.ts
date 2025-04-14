1ab3bbfc47f7804885f87bc2f2ca7b3a7c921bbe
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-04-14 17:23:19 +0530

    feat(user): add isAdmin field to User model and update related schema

--- keystone.ts
+++ keystone.ts
@@ -1,1 +1,19 @@
-  
+import { config } from "@keystone-6/core";
+import { withAuth, session } from "./auth";
+import { User } from "./schemas/user";
+
+export default withAuth(
+  config({
+    db: {
+      provider: "sqlite",
+      url: "file:./db.sqlite",
+    },
+    lists: { User },
+    session,
+    ui: {
+      isAccessAllowed: ({ session }) => {
+        return !!session?.data?.isAdmin;
+      },
+    },
+  })
+);
